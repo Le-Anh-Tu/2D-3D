8e96e993ebf52a213702bab0d79f3bc2d95f57dd
le tu <leanhtu280295@gmail.com>
2021-08-11 21:46:14 +0700

    vao

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,12 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(3,5);
+        System.out.println("Point 2D : "+point2D.toString());
+
+        Point3D point3D = new Point3D(5,6,7);
+        System.out.println("Point 3D : " + point3D.toString());
+    }
+}
